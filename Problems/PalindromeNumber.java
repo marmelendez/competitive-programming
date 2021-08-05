0d0243bed9a4406da670877bdb963ccef2506591
Maribel Melendez <lizbethmelendez@live.com>
2021-08-05 14:21:13 -0500

    Test isPalindrome function

--- Problems/PalindromeNumber.java
+++ Problems/PalindromeNumber.java
@@ -1,7 +1,7 @@
 package Problems;
 
 public class PalindromeNumber {
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         String xStr = String.valueOf(x);
         int startIndex = 0;
         for (int i = xStr.length() -1 ; i >= 0; i--) {
@@ -12,8 +12,10 @@ public class PalindromeNumber {
             }
         }
         return true;
+    }   
+
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x));
     }
-
-
-    
 }
