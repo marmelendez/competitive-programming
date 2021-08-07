f818a8857265ddb954257ad94c5f28509d743157
Maribel Melendez <lizbethmelendez@live.com>
2021-08-06 22:22:04 -0500

    Test function remove element

--- Problems/RemoveElements.java
+++ Problems/RemoveElements.java
@@ -1,18 +1,20 @@
 package Problems;
 
 public class RemoveElements {
-    public int removeElement(int[] nums, int val) {
+    public static int removeElement(int[] nums, int val) {
         int k = 0;
-        int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[index] = nums[i];
-                index ++;
-                //agregar a nums
+                nums[k] = nums[i];
+                k ++;
             }
         }
-
         return k;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        System.out.println(removeElement(nums,val));
     }
     
 }
