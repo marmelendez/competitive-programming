2852d48a3987d9dc09ff4460229b03df00602472
Maribel Melendez <lizbethmelendez@live.com>
2021-08-09 17:56:40 -0500

    Solve index bug

--- Problems/ReverseInt.java
+++ Problems/ReverseInt.java
@@ -1,16 +1,21 @@
 package Problems;
 
 public class ReverseInt {
-    public int reverse(int x) {
+    public static int reverse(int x) {
         String xStr = String.valueOf(x);
         String resStr = "";
         int res;
-        for (int i = xStr.length() ; i >= 0; i--) {
+        for (int i = xStr.length() - 1; i >= 0; i--) {
             resStr += xStr.charAt(i);
         }
 
         res = Integer.valueOf(resStr);
 
         return res;        
+    }
+
+    public static void main(String[] args) {
+        int x = 123;
+        System.out.println(reverse(x));
     }
 }
