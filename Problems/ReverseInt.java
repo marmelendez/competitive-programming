c864ceaad5b3d020e2209cacc2997d3aa73975cf
Maribel Melendez <lizbethmelendez@live.com>
2021-08-09 19:03:45 -0500

    Solve reverse int

--- Problems/ReverseInt.java
+++ Problems/ReverseInt.java
@@ -30,7 +30,6 @@ public class ReverseInt {
             int digit = x % 10;
             res = res * 10 + digit;
             x /= 10;
-            System.out.println(res);
         }
         if (res < Math.pow(-2, 31) || res > Math.pow(2, 31) - 1) {
             return 0;
@@ -40,7 +39,7 @@ public class ReverseInt {
 
 
     public static void main(String[] args) {
-        int x = 1534236469;
+        int x = -120;
         System.out.println(alternativeSolution(x));
     }
 }
