b0d786be27ddf13483e76d58b0f9f46118f09bee
Maribel Melendez <57516503+marmelendez@users.noreply.github.com>
2021-08-17 22:55:52 -0500

    Implement posible solution

--- Challenge/ProblemaA.java
+++ Challenge/ProblemaA.java
@@ -1,14 +1,37 @@
 package Challenge;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ProblemaA {
 
-    public void getCoins() {
+    public int getCoins(int n, int[] values) {
+        //int nCoins = 0;
+        Arrays.sort(values);
 
+        int sum = 0;
+        int sumCoins = 0;
+        int count = 0;
+
+        for (int i = 0; i < values.length; i++){
+            sum += values[i];
+        }
+
+        do {
+            count ++;
+            sum = sum - values[values.length-count];
+            sumCoins += values[values.length-count];
+        } while (sum >= sumCoins);
+
+        return count;
     }
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        ProblemaA sol = new ProblemaA();
+        int[] arr = {1,2,3,4};
+        System.out.println(sol.getCoins(4, arr));
+        
+        /*Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int k = s.nextInt();
         int count = 0;
@@ -17,8 +40,7 @@ public class ProblemaA {
             if (x % k == 0)
                 count++;
         }
-        System.out.println(count);
-        
+        System.out.println(count);*/
     }
     
 }
