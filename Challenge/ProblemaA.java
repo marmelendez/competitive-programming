2bec090cbc0efa4586ed705193de94029a1c9b02
Maribel Melendez <lizbethmelendez@live.com>
2021-08-18 21:38:18 -0500

    Edit java problema A

--- Challenge/ProblemaA.java
+++ Challenge/ProblemaA.java
@@ -1,11 +1,7 @@
 package Challenge;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-//import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class ProblemaA {
 
@@ -34,32 +30,6 @@ public class ProblemaA {
         ProblemaA sol = new ProblemaA();
         int[] arr = {3,3};
         System.out.println(sol.getCoins(2, arr));
-       /* BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter n: "); 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        System.out.print("Enter k: "); 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int count = 0;
-        while (n-- > 0) {
-            int x = Integer.parseInt(br.readLine());
-            if (x % k == 0)
-                count++;
-        }
-        System.out.println(count);
-        
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int k = s.nextInt();
-        int count = 0;
-        while (n-- > 0) {
-            int x = s.nextInt();
-            if (x % k == 0)
-                count++;
-                test this 
-        }
-        System.out.println(count);*/
     }
     
 }
