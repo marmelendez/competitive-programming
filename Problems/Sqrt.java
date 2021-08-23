2049b5c05d64136351b223843872f8c2ba42ad72
Maribel Melendez <lizbethmelendez@live.com>
2021-08-22 22:34:45 -0500

    Test sqrt problem

--- Problems/Sqrt.java
+++ Problems/Sqrt.java
@@ -2,7 +2,10 @@ package Problems;
 
 class Sqrt {
     public int mySqrt(int x) {
-        for (int i = 1; i < x; i++) {
+        for (int i = 0; i < x; i++) {
+            if (i * i == x) {
+                return i;
+            }
             if (i * i > x) {
                 return i-1;
             }
@@ -12,6 +15,11 @@ class Sqrt {
     }
 
     public static void main(String[] args) {
-        
+        Sqrt sol = new Sqrt();
+        System.out.println(sol.mySqrt(0));
+        System.out.println(sol.mySqrt(3));
+        System.out.println(sol.mySqrt(4));
+        System.out.println(sol.mySqrt(5));
+        System.out.println(sol.mySqrt(16));
     }
 }
