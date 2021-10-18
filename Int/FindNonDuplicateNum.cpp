ac224eeb8fba982e86ecfc0660a41cde313a1fa6
Maribel Melendez <lizbethmelendez@live.com>
2021-10-17 20:35:56 -0500

    Find non duplicate number from an integer array

---
+++ Int/FindNonDuplicateNum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int findNonDuplicate(vector<int> arrayNums) {
+    unordered_set<int> numbers;
+    int sum =  0;
+
+    for (int i = 0; i < arrayNums.size(); i++) {
+        if (numbers.find(arrayNums[i]) == numbers.end()) { //not found
+            numbers.insert(arrayNums[i]);
+            sum += arrayNums[i];
+        } else { // found
+            sum -= arrayNums[i];
+        }
+    }
+
+    return sum;
+}
+
+
+int main() {
+    vector<int> numbers;
+    numbers.push_back(2);
+    numbers.push_back(2);
+    numbers.push_back(3);
+    numbers.push_back(4);
+    numbers.push_back(7);
+    numbers.push_back(4);
+    numbers.push_back(3);
+
+    cout << findNonDuplicate(numbers) << endl;
+}
